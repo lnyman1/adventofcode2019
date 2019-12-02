a316e7ed7926cd869bc4a30cb26ea4d232f833c4
lnyman1 <laranyman92@gmail.com>
2019-12-02 18:57:48 -0500

    Factoring out opcode for use in later questions

---
+++ 2019/3/3.py
@@ -0,0 +1,30 @@
+import os
+import requests
+import requests_cache
+
+
+def part1(lines):
+    for line in lines:
+        print(line)
+
+
+def part2(lines):
+    for line in lines:
+        print(line)
+
+def get_input_file():
+    requests_cache.install_cache('../cache')
+    url = 'https://adventofcode.com/' + os.path.abspath(__file__).split('/')[-3] + '/day/3/input'
+    lines = requests.get(url, cookies={"session": os.environ['SESSION']}).text.strip().splitlines()
+    return lines
+
+
+def main():
+    lines = get_input_file()
+
+    print(part1(lines))
+    print(part2(lines))
+
+
+if __name__ == "__main__":
+    main()
